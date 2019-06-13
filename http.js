244507019dcb1fed22826e0573bdf5c6ad678419
zjj <760612141@qq.com>
2019-06-13 11:23:33 +0800

    first

---
+++ http.js
@@ -0,0 +1,11 @@
+const http = require('htttp');
+const hostname = "127.0.0.1";
+const port = 3000;
+const server = http.createServer((res, req) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain')
+    res.end('Hello World\n')
+})
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`)
+  })
